ed98466e58369f4bbf757c6949b9a3511f94b66f
willys <w1llys_910710@yahoo.co.id>
2021-03-11 19:50:24 +0700

    owl courosel update

--- resources/views/sections/youtube.blade.php
+++ resources/views/sections/youtube.blade.php
@@ -11,7 +11,7 @@
 <section class="home_banner_area">
     {{-- <div class="banner_inner d-flex align-items-center"> --}}
         <div class="overlay bg-parallax" data-stellar-ratio="0.9" data-stellar-vertical-offset="0" data-background=""></div>
-        <iframe width="50%" height="50%" src="https://www.youtube.com/embed/LHXh6w1dpB0?rel=0;&autoplay=1" frameborder="0" allowfullscreen class="video"></iframe>
+        <iframe width="50%" height="50%" src="https://www.youtube.com/embed/o2VgyME5xGs?rel=0;&autoplay=1" frameborder="0" allowfullscreen class="video"></iframe>
         {{-- <script src="https://apps.elfsight.com/p/platform.js" defer></script>
         <div class="elfsight-app-acb2ee68-e9b7-4e57-b751-150d41606123"></div> --}}
     {{-- </div> --}}
