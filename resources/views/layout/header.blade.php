624e92485e10cb5276021d69c68cabeea2990de3
willys <w1llys_910710@yahoo.co.id>
2021-04-08 21:22:54 +0700

    code improvement

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,5 +1,4 @@
 <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
@@ -8,19 +7,16 @@
     <meta name="author" content="Tangan Pengharapan (W)">
     <meta name="HandheldFriendly" content="true" />
     <meta name="google-site-verification" content="wjckL_jYqSFyPC6Dv0tfAe7kg9Pmp__7EQLuyZU4L-s" />
-    {{-- <link rel="icon" href="/img/favicon.png" type="image/png"> --}}
     <link rel="shortcut icon" type="/image/ico" href="/img/icon.ico" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Tangan Pengharapan</title>
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="/css/bootstrap.css">
-    <link rel="stylesheet" href="/vendors/linericon/style.css">
-    <link rel="stylesheet" href="/css/font-awesome.min.css">
-    <link rel="stylesheet" href="/vendors/owl-carousel/owl.carousel.min.css">
-    <link rel="stylesheet" href="/vendors/lightbox/simpleLightbox.css">
-    <link rel="stylesheet" href="/vendors/nice-select/css/nice-select.css">
-    <link rel="stylesheet" href="/vendors/animate-css/animate.css">
-    <!-- main css -->
-    <link rel="stylesheet" href="/css/style.css">
-    <link rel="stylesheet" href="/css/responsive.css">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+    <link rel="stylesheet" href="{{asset('vendors/linericon/style.css')}}">
+    <link rel="stylesheet" href="{{asset('css/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{asset('vendors/owl-carousel/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{asset('vendors/lightbox/simpleLightbox.css')}}">
+    <link rel="stylesheet" href="{{asset('vendors/nice-select/css/nice-select.css')}}">
+    <link rel="stylesheet" href="{{asset('vendors/animate-css/animate.css')}}">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('css/responsive.css')}}">
 </head>
